ec3fe459d1356e6d7f45fad57dc33309301a1ae6
Vincent Herlemont <vincentherl@leszeros.com>
2020-08-24 19:05:29 +0000

    resources generator

--- next.config.js
+++ next.config.js
@@ -3,5 +3,10 @@ const withPWA = require('next-pwa')
 module.exports = withPWA({
  pwa: {
   dest: 'public'
+ },
+ rewrites() {
+  return [
+   { source: '/_next/:path*', destination: '/toto/:path*' }
+  ]
  }
 })
--- pages/index.js
+++ pages/index.js
@@ -5,50 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Captweet</title>
         <link rel="icon" href="/favicon.ico" />
         <link rel="manifest" href="/manifest.json"/>
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js! 4</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <div className={styles.dynamic_test}> Static CSS Test</div>
       </main>
 
       <footer className={styles.footer}>
