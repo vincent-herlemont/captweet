b38058a0764c905b0415d2bdee76aa64b87bf627
Vincent Herlemont <vincentherl@leszeros.com>
2020-08-25 12:45:53 +0000

    init components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
 import '../styles/globals.css'
+import theme from '../styles/theme'
+import {ThemeProvider} from "styled-components";
+
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  console.log(theme);
+  return <ThemeProvider theme={theme} >
+    <Component {...pageProps} />
+  </ThemeProvider>
 }
 
 export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,25 @@
 import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: ${(props) => props.theme.color.yellow};
+`
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Captweet</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="manifest" href="/manifest.json"/>
-      </Head>
+    <div>
+     <Head>
+      <title>Captweet</title>
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="manifest" href="/manifest.json"/>
+     </Head>
 
-      <main className={styles.main}>
-        <div className={styles.dynamic_test}> Static CSS Test</div>
+      <main>
+        <Title>Capteet</Title>
+        <div>Static CSS Test</div>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
+      <footer>
       </footer>
     </div>
   )
